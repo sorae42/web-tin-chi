f5a86b5b39aeeb22ed490ea2a2746e9d2ce16b15
Sora <34794115+sorae42@users.noreply.github.com>
2023-05-08 22:32:58 +0700

    có database mới

---
+++ sql/init.sql
@@ -0,0 +1,45 @@
+-- this query needs to be executed before doing anything else.
+
+CREATE DATABASE SchoolDatabase;
+
+USE SchoolDatabase;
+
+-- sinh viên
+CREATE TABLE IF NOT EXISTS students (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(24) NOT NULL,
+    password VARCHAR(100),
+
+    real_name VARCHAR(50) NOT NULL,
+    gender VARCHAR(3) NOT NULL DEFAULT FALSE,
+    hometown VARCHAR(100), 
+
+    istutor BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO students VALUES ('0', 'qnu', '0', 'Quy Nhon University', 'Nam', 'Quy Nhon, Tinh Binh Dinh', false, DEFAULT);
+
+-- môn học
+CREATE TABLE IF NOT EXISTS subjects (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(24) NOT NULL,
+    display_name VARCHAR(64),
+    
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO subjects VALUES ('0', 'demo', 'Thử nghiệm', DEFAULT);
+
+-- danh sách sinh viên đã đăng ký
+CREATE TABLE IF NOT EXISTS registered(
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    student_id INT(6) UNSIGNED NOT NULL,
+    subject_id INT(6) UNSIGNED NOT NULL,
+    registered_on DATETIME DEFAULT CURRENT_TIMESTAMP, 
+
+    FOREIGN KEY(student_id) REFERENCES students(id),
+    FOREIGN KEY(subject_id) REFERENCES subjects(id)
+);
+
+INSERT INTO registered VALUES ('0', '0', '0', DEFAULT);
