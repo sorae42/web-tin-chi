fc2fa1c4d121796dd4bdce12ea7db09066cc0ac1
Sora <34794115+sorae42@users.noreply.github.com>
2023-05-11 14:32:12 +0700

    người dùng có thể đổi mật khẩu

---
+++ template/account.php
@@ -0,0 +1,58 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    session_start();
+
+    $username = $_SESSION['name'];
+    $password = $_POST['password'];
+    $confirm = $_POST['password-confirm'];
+    
+    include 'utils/db.php';
+
+    if (empty($password) || $password != $confirm) {
+        showMessage("Mật khẩu không trùng khớp!!");
+    } else {
+        $db = new db();
+        $db->query("UPDATE users SET password = ? WHERE username = ?", $password, $username); 
+
+        $db->close();
+    }
+}
+
+$permission = match ($_SESSION['permission']) {
+    0 => 'Sinh viên',
+    1 => 'Giảng viên',
+    2 => 'Quản trị viên'
+}
+?>
+
+<div class="container">
+    <h2>Xin chào,
+        <?= $_SESSION['displayName'] ?>!
+    </h2>
+
+    <p>Bạn là <?= $permission ?>!</p> 
+
+    <a href="classregister.php">
+        <button>
+            <?= ($_SESSION['permission'] == 0 ? "Đăng ký" : "Quản lý") ?> học phần
+        </button>
+    </a> 
+
+    <p>Bạn có thể đổi mật khẩu tài khoản tại đây</p>
+    <form action="<?= $_SERVER['PHP_SELF']; ?>" method="post">
+        <div class="form-group">
+            <label for="password">Mật khẩu</label>
+            <input type="password" name="password" id="password" required />
+        </div>
+        <div class="form-group">
+            <label for="password-confirm">Nhập lại mật khẩu</label>
+            <input type="password" name="password-confirm" id="password-confirm" required />
+        </div>
+        <input type="submit" id="submit-btn" value="Đổi mật khẩu" />
+    </form>
+
+    <a href="/logout.php">
+        <button>Đăng xuất tài khoản</button>
+    </a>
+
+</div>
